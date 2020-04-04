0f5e3da6cad23ad037b013cf843e6a745183b268
Luke DeRienzo <javasoftwarearchitect@gmail.com>
2020-04-03 23:08:56 -0400

    Rolled back file reading code in TacoFileDataSource.java.

--- src/main/java/com/pwvconsultants/tacosspring/data/TacoFileDataSource.java
+++ src/main/java/com/pwvconsultants/tacosspring/data/TacoFileDataSource.java
@@ -2,6 +2,9 @@ package com.pwvconsultants.tacosspring.data;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,7 +15,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pwvconsultants.tacosspring.model.Taco;
 import com.pwvconsultants.tacosspring.model.Tacos;
-import com.pwvconsultants.tacosspring.utils.FileReader;
 
 
 public final class TacoFileDataSource {
@@ -24,7 +26,6 @@ public final class TacoFileDataSource {
     private Taco[] tacos;
     private ObjectMapper mapper;
 
-
     public TacoFileDataSource(String filePath) {
         this.filePath = filePath;
         mapper = new ObjectMapper();
@@ -32,7 +33,7 @@ public final class TacoFileDataSource {
     }
 
     private Taco[] readTacoDataFromFile(ObjectMapper mapper) {
-        tacosJsonString = getTacosJsonStringFromFile();
+        tacosJsonString = readFileContents();
         try {
             return mapper.readValue(tacosJsonString, Taco[].class);
         } catch (JsonProcessingException e) {
@@ -40,9 +41,19 @@ public final class TacoFileDataSource {
         }
     }
 
-    private String getTacosJsonStringFromFile() {
-        StringBuilder fileContents = FileReader.readFileContents(filePath);
-        return parseOutTacosJsonArray(fileContents);
+    private String readFileContents() {
+        String absoluteFilePath = new File(filePath).getAbsolutePath();;
+        return getTacosJsonStringFromAbsolutePath(absoluteFilePath);
+    }
+
+    private String getTacosJsonStringFromAbsolutePath(String absoluteFilePath) {
+        StringBuilder contentBuilder = new StringBuilder();
+        try (Stream<String> stream = Files.lines(Paths.get(absoluteFilePath), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return parseOutTacosJsonArray(contentBuilder);
     }
 
     private String parseOutTacosJsonArray(StringBuilder contentBuilder) {
@@ -52,7 +63,7 @@ public final class TacoFileDataSource {
     }
 
     public String getTacosJsonString() {
-        tacosJsonString = getTacosJsonStringFromFile();
+        tacosJsonString = readFileContents();
         return tacosJsonString;
     }
 
